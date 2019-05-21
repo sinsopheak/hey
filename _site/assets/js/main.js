8aa181832ce0324ba3733b3d821535e1ba68f83f
sinsopheak <sin.sopheak@outlook.com>
2019-05-21 21:45:25 +0700

    removed unused

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,3 +4,28 @@ $(document).ready(function () {
 
 var windowh = window.innerHeight;
 document.getElementsByClassName('header').style.height = windowh;
+
+$(document).ready(function () {
+    $.ajax({
+        url: 'https://www.billboard.com/rss/charts/hot-100',
+        type: 'GET',
+        dataType: "xml"
+    }).done(function (xml) {
+
+        $.each($("item", xml), function (i, e) {
+
+            var blogNumber = i + 1 + ". ";
+
+            var itemURL = ($(e).find("link"));
+            var blogURL = "<a href='" + itemURL.text() + "'>" + itemURL.text() + "</a>";
+
+            var itemTitle = ($(e).find("title"));
+            var blogTitle = "<h4>" + blogNumber + itemTitle.text() + "</h4>";
+
+            $("#feed").append(blogTitle);
+            $("#feed").append(blogURL);
+            console.log("Hello");
+
+        });
+    });
+});
